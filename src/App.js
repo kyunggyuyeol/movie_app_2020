21e7596bc13f0781025ea5351bb136122604ed3b
Gyu_Yeol Kyung <p82468@paran.com>
2020-09-06 13:42:56 +0900

    afals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+function Food(prop)
+{
+  return <h1>I like {prop.fav}</h1>;
+}
+
 function App() {
-  return  <div className="App"/>
+  return (<div>
+    <h1>Hello</h1>
+    <Food fav="Kimchi" something={true} papapa={['hello', 1, 2, 3, 4, 5, true]} />
+  </div>); 
 }
 
 export default App;
